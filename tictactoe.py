383d0a21d3c22b89ada6bb9bf44b1fb63df1d886
Thakdanai <Thakdanai@localhost>
2016-10-20 04:43:17 +0700

    click to check position

--- tictactoe.py
+++ tictactoe.py
@@ -9,13 +9,25 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 
 class Tictactoe(AnchorLayout):
-    pass   
+    def on_touch_down(self, touch):
+        self.checkPos(touch)
+        return super(Tictactoe, self).on_touch_down(touch)
+
+    def checkPos(self,touch):
+        for i in range (0,9):
+            sizeX = self.width/6    # ความกว้างของช่อง
+            sizeY = self.height/6   # ความยาวของช่อง
+            x = (self.width/4) + ((i % 3)*sizeX)    # self.width/4 คือ ตำแหน่งซ้ายสุดของตาราง | i%3 เพื่อเช็คว่าอยู่ row ไหน
+            y = (self.height/4) + ((i // 3)*sizeY)  # self.heigth/4 คือ ตำแหน่งล่างสุดของตาราง | i//3 เพื่อเช็คว่าอยู่ columm ไหน
+            if(touch.x > x and touch.x < x+sizeX and touch.y > y and touch.y < y+sizeY):
+                print(i+1)
+        
 
 class StatusBar(BoxLayout):
     pass
 
 class Board(GridLayout):
-    pass  
+    pass
     
 class Option(BoxLayout):
     pass
